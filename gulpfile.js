dc44f9877eafee4055099aefcb8f63a2ea5bc108
ShubhamKD29 <debnathshubham38@gmail.com>
2024-07-05 15:53:47 +0530

    json updated

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,39 @@
+// const gulp = require('gulp');
+// const requireDir = require('require-dir');
+
+// requireDir('./gulp');
+
+// /*=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// |  Compile
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
+// gulp.task('compile', gulp.parallel('style', 'script', 'script:webpack', 'vendor'));
+// gulp.task('compile:all', gulp.parallel('compile', 'pug'));
+
+// /*=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// |  Deploy
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
+// gulp.task('build', gulp.series('clean:build', 'build:static', 'compile:all'));
+// gulp.task('build:test', gulp.series('build', 'watch'));
+// gulp.task('live', gulp.series('clean:live', 'build', 'build:push'));
+
+// /*=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// |  Run development environment
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
+// gulp.task('default', gulp.series('clean', 'compile', 'watch'));
+
+
 const gulp = require('gulp');
-const requireDir = require('require-dir');
+const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
 
-requireDir('./gulp');
+gulp.task('sass', function() {
+  return gulp.src('./src/scss/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./dist/css'));
+});
 
-/*=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-|  Compile
-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
-gulp.task('compile', gulp.parallel('style', 'script', 'script:webpack', 'vendor'));
-gulp.task('compile:all', gulp.parallel('compile', 'pug'));
-
-/*=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-|  Deploy
-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
-gulp.task('build', gulp.series('clean:build', 'build:static', 'compile:all'));
-gulp.task('build:test', gulp.series('build', 'watch'));
-gulp.task('live', gulp.series('clean:live', 'build', 'build:push'));
-
-/*=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-|  Run development environment
-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
-gulp.task('default', gulp.series('clean', 'compile', 'watch'));
+gulp.task('default', gulp.series('sass'));
